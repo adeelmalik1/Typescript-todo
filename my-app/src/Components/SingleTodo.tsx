d59c774e035b20a884faeef6ac1c6ab8099d595a
Adeelmalik1 <adeeladi0014@gmail.com>
2022-05-07 08:21:42 +0500

    final-code

--- my-app/src/Components/SingleTodo.tsx
+++ my-app/src/Components/SingleTodo.tsx
@@ -23,8 +23,10 @@ const SingleTodo: React.FC<props> = ({ todos, todo, setTodos }) => {
         <ul>
             <li key={todo.id}>
             <h3 style={todo.isDone ? { textDecoration: 'line-through' } : {}}>{todo.todo}</h3>
-            <button onClick={() => handleDelete(todo.id)}>Delete</button>
-            <button onClick={() => handleComplete(todo.id)}>complete</button>
+            <div className='btn'>
+            <button id="btn-1" onClick={() => handleDelete(todo.id)}>Delete</button>
+            <button id="btn-2" onClick={() => handleComplete(todo.id)}>complete</button>
+            </div>
             </li>
         </ul>
     )
